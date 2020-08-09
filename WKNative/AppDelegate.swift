f3c73729fe7242407fa8ee284f8f3690e3b04722
Justin Bush <revblaze@users.noreply.github.com>
2020-08-09 17:29:41 -0400

    Add base macOS project

---
+++ WKNative/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WKNative
+//
+//  Created by Justin Bush on 2020-08-09.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
