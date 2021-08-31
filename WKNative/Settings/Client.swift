641ec68daa9c1d78e8458aebea2bd6522b56acff
Justin Bush <revblaze@users.noreply.github.com>
2021-08-31 11:04:51 -0400

    Update Client.swift

--- WKNative/Settings/Client.swift
+++ WKNative/Settings/Client.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct Client {
     
-    static let userAgent = ""
+    static let userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.2 Safari/605.1.15"
     
 }
 
